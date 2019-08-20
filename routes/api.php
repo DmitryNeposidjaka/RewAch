9d154cd63a257d7912e540869c3a492b4b462229
dmitry-neposidyaka <dmitry.neposidyaka@uadevelopers.com>
2019-08-20 18:20:11 +0300

    init auth

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->namespace('Api')->group(function () {
+    Route::post('login', 'LoginController')->middleware('throttle:10,1')->name('login');
+    Route::post('logout', 'LogoutController')->middleware('auth:api')->name('logout');
+});
