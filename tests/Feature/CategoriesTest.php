78ba68e1766080635e47f085baec46a3ba2918a0
dmitry-neposidyaka <dmitry.neposidyaka@uadevelopers.com>
2019-09-04 12:45:41 +0300

    categories tests complete

--- tests/Feature/CategoriesTest.php
+++ tests/Feature/CategoriesTest.php
@@ -44,11 +44,51 @@ class CategoriesTest extends TestCase
         array_push($this->afterApplicationCreatedCallbacks, $achievementCreate, $setUser, $categoryCreate);
     }
 
+    public function testCreateCategorySuccess()
+    {
+        $response = $this->actingAs($this->user, 'api')->post(route('category.create'), [
+            'name' => $this->faker->name
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function testUpdateCategorySuccess()
+    {
+        $response = $this->actingAs($this->user, 'api')->post(route('category.update', [
+            'category' => $this->category->id
+        ]), [
+            'name' => $this->faker->name
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function testDeleteCategorySuccess()
+    {
+        $response = $this->actingAs($this->user, 'api')->delete(route('category.delete', [
+            'category' => $this->category->id
+        ]));
+
+        $response->assertStatus(204);
+    }
+
     public function testAttachCategoryToAchievementSuccess()
     {
         $response = $this->actingAs($this->user, 'api')->post(route('achievement.attach.category', [
             'achievement' => $this->achievement->id,
-            'category' => $this->category->id,
+            'entity' => $this->category->id,
+        ]));
+
+        $response->assertStatus(200);
+    }
+
+    public function testDetachCategoryToAchievementSuccess()
+    {
+        $this->achievement->categories()->attach($this->category->id);
+        $response = $this->actingAs($this->user, 'api')->delete(route('achievement.detach.category', [
+            'achievement' => $this->achievement->id,
+            'entity' => $this->category->id,
         ]));
 
         $response->assertStatus(200);
